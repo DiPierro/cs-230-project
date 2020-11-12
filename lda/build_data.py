74abdb6e270fb5065b7b4e5b789fcd682ac2c2bc
DiPierro <adipier1@gmail.com>
2020-11-11 19:41:52 -0800

    adding lda

---
+++ lda/build_data.py
@@ -0,0 +1,244 @@
+"""build_data.py"""
+# Libraries------------------
+
+# import pydiff
+import difflib
+import os
+# import pandas as pd
+from pprint import pprint
+import pdb
+import re
+from collections import Counter
+import spacy
+import csv
+from operator import itemgetter 
+import random
+
+# Code-----------------------
+
+def preprocess(file_list, places):
+    """Processes all of the documents."""
+    # TODO: Add bigrams, phrases, etc from Gensim
+    # TODO: threshhold with Gensim
+    # See: https://radimrehurek.com/gensim/auto_examples/tutorials/run_lda.html#sphx-glr-auto-examples-tutorials-run-lda-py
+    print("Preprocessing documents...")
+    new_list = []
+    
+    for place in places:
+        print("Processing ", place)
+        baseline_flag = False
+        baseline_checked = False
+        counter = 0
+        for doc in file_list:
+            if doc['place'] == place and not baseline_flag:
+                baseline = doc
+                baseline_flag = True
+            elif doc['place'] == place and baseline_flag:
+                counter += 1
+                if not baseline_checked:
+                    baseline['processed_text'] = process_text(text = diff_doc(baseline=doc['raw_text'], new_doc=baseline['raw_text']))
+                    assert 'doc_id' in baseline.keys()
+                    new_list.append(baseline)
+                    print("Added ", baseline['doc_id'])
+                    baseline_checked = True
+                doc['processed_text'] = process_text(text = diff_doc(baseline=baseline['raw_text'], new_doc=doc['raw_text']))
+                assert 'doc_id' in doc.keys()
+                new_list.append(doc)
+                # print("Added ", doc['doc_id'])
+        # Add any document that can't be diffed
+        if counter == 0:
+            baseline['processed_text'] = process_text(text = ''.join(baseline['raw_text']))
+            assert 'doc_id' in baseline.keys()
+            new_list.append(baseline)
+
+    print("Finished preprocessing documents.")
+
+    return new_list
+
+def read_files(directory):
+    """Read in text files to be processed."""
+    print("Reading files...")
+    file_list = []
+    places = []
+    files = os.listdir(DIR)[50:]
+    
+    for index, item in enumerate(files):
+        
+        # Add docs to file_list
+        file_dict = {}
+        
+        file_name = directory + "/" + files[index]
+        file_dict['raw_text'] = open(file_name, 'r').read().splitlines()
+        file_dict['doc_id'] = item
+        file_dict['place'] = re.search(r"^.*(?=_20)", item).group(0)
+        file_dict['date'] = re.search(r"20\d{2}-\d{2}-\d{2}(?=_)", item).group(0)
+        file_dict['doc_title'] = re.search(r"(?<=\d{2}_).*(?=_\w+)", item).group(0)
+        file_dict['doc_type'] = re.search(r"(Agenda|Minutes)(?=\.txt)", item).group(0)
+        assert 'doc_id' in file_dict.keys()
+        file_list.append(file_dict)
+        # Add places to list of places
+        if file_dict['place'] not in places:
+            places.append(file_dict['place'])
+    assert len(files) == len(file_list)
+    print("Finished reading files.")
+    return places, file_list
+        
+def diff_doc(baseline, new_doc):
+    """Compare a given doc to a baseline doc to remove boilerplate text."""
+    d = difflib.Differ()
+    result = list(d.compare(baseline, new_doc))
+    results = []
+    for line in result:
+        if line.startswith('+'):
+            results.append(line[1:])
+    return ' '.join(results)
+
+def process_text(text):
+    """Remove useless POS and stop words, lemmatize, lowercase."""
+    # Setup
+
+    sp_text = nlp(text)
+    pos_list = ['AUX', 'ADP', 'CONJ', 'CCONJ', 'DET', 'NUM', 'PART', 'PRON', 'PUNCT', 'SYM', 'SPACE', 'X']
+
+    # Part of speech filter
+    pos_filter = []
+    pos_tags = [(word, word.pos_) for word in sp_text]
+    for tag in pos_tags:
+        if tag[1] not in pos_list:
+            pos_filter.append(str(tag[0]).lower())
+    
+    # Identify entities
+    ents = set()
+    for ent in sp_text.ents:
+        if ent.label_ in ["PERSON"]:
+            ents.add(str(ent).lower())
+    
+    # Remove stopwords
+    default_stops = nlp.Defaults.stop_words
+    default_stops |= {
+        'agenda','minutes','council','meeting','planning','mayor','ordinance','district',
+        'special','county','city','municipal','commission','committee',
+        'amendment','district','https','notice','event','staff','comment' 
+        'participate','report','doc_text','report','advisory','name','team',
+        'printed', 'public', 'public hearing', 'office', 'manager', 'supervisor', 
+        'board', 'clerk', 'motion', 'consent', 'calendar', 'finance', 'printed'
+        'record', 'code', 'section', 'attorney', 'staff', 'report', 'item', 
+        'clerk', 'chair', 'page', 'meet', 'plan', 'call', 'participate', 'ada',
+        'civil', 'remotely', 'consider', 'library', 'budget', 'commissions'
+    }
+    default_stops |= ents
+    # TODO: Make this a portable path
+    default_stops |= add_stop_words('/Users/amydipierro/GitHub/cs230/nlp/data/processed/top_words.csv')
+    
+    lower = [word.lower() for word in pos_filter]
+    no_stops = [word for word in lower if word not in default_stops]
+
+    no_short = [word for word in no_stops if len(word) != 1]
+    
+    return no_short
+
+def add_stop_words(word_file):
+    """Reads csv file of 200 most-common words into list."""
+    csv_top_words = open(word_file)
+    top_words = csv.reader(csv_top_words)
+    top_words_set = set()
+    for word in top_words:
+        top_words_set.add(word[0])
+    return top_words_set
+
+def get_top_words(file_list):
+    """Counts the most common words in our corpus."""
+    word_dict = {}
+    print("Getting top words...")
+    for doc in file_list:
+        for word in doc['diff_text']:
+            if word not in word_dict:
+                word_dict[word] = 1
+            else:
+                word_dict[word] = word_dict[word] + 1
+    top_200 = dict(sorted(word_dict.items(), key = itemgetter(1), reverse = True)[:200])
+    print("Finished getting top words.")
+
+    # TODO: Make this a portable path
+    with open('/Users/amydipierro/GitHub/cs230/nlp/data/processed/top_words.csv', 'w') as csv_file:  
+        writer = csv.writer(csv_file)
+        for key, value in top_200.items():
+            writer.writerow([key])
+
+def chunk(processed_files, num_words):
+    """Splits each document into chunks of between len(num_words) to len(num_words*1.5)"""
+    new_list = []
+    for doc in processed_files:
+        assert ('doc_id' and 'processed_text') in doc.keys()
+        full_text = doc['processed_text']
+        for i in range(0, len(full_text), num_words):
+            # Check how many words will be left in the doc after this chunk
+            if len(full_text[i:i + num_words*2]) > (num_words*1.5):
+                new_dict = {}
+                new_dict['doc_id'] = doc['doc_id']
+                new_dict['place'] = doc['place']
+                new_dict['date'] = doc['date']
+                new_dict['doc_title'] = doc['doc_title']
+                new_dict['doc_type'] = doc['doc_type']
+                new_dict['doc_text'] = full_text[i:i + num_words]
+                assert ('doc_id' and 'doc_text') in new_dict.keys()
+                new_list.append(new_dict)
+            # If there will be 50 or fewer words left in the doc
+            # then combine the next chunk with the current chunk
+            else:
+                new_dict = {}
+                new_dict['doc_id'] = doc['doc_id']
+                new_dict['place'] = doc['place']
+                new_dict['date'] = doc['date']
+                new_dict['doc_title'] = doc['doc_title']
+                new_dict['doc_type'] = doc['doc_type']
+                new_dict['doc_text'] = full_text[i:i + num_words*2]
+                assert ('doc_id' and 'doc_text') in new_dict.keys()
+                new_list.append(new_dict)
+    print("Finished chunking text.")
+    return new_list
+
+def split(final_files):
+    """Split data into train/dev/test sets."""
+    assert 'doc_id' in final_files[0].keys()
+    final_files = sorted(final_files, key = lambda i: i['doc_id'])
+    random.seed(230)
+    random.shuffle(final_files)
+    split_1 = int(0.8 * len(final_files))
+    split_2 = int(0.9 * len(final_files))
+    train = final_files[:split_1]
+    dev = final_files[split_1:split_2]
+    test = final_files[split_2:]
+    return train, dev, test
+
+def write(data, name):
+    """Helper method to write train/dev/test sets to csv files."""
+    # TODO: Make this a portable path
+    print("Writing ", name, "data...")
+    file_name = '/Users/amydipierro/GitHub/cs230/nlp/data/processed/' + name + '/' + name + '_200.csv'
+    headers = data[0].keys()
+    with open(file_name, 'w') as csv_file:
+        dict_writer = csv.DictWriter(csv_file, headers, delimiter='\t')
+        dict_writer.writeheader()
+        dict_writer.writerows(data)
+
+if __name__ == '__main__':
+    # TODO: Turn into a CLI
+    # TODO: Make this a portable path
+    # TODO: Add logging (esp. for length of data)
+    
+    DIR = "/Users/amydipierro/GitHub/cs230/nlp/data/raw/content/docs"
+    nlp = spacy.load('en_core_web_sm') # TODO: Identify this as a requirement
+    places, file_list = read_files(directory=DIR)
+    processed_files = preprocess(file_list, places)
+    assert len(file_list) == len(processed_files)
+    final_files = chunk(processed_files, 200)
+    train, dev, test = split(final_files)
+    write(train, "train")
+    write(dev, "dev")
+    write(test, "test")
+    
+    # TODO: move this to its own script
+    # get_top_words(processed_files)
+    
+
---
+++ lda/lda.py
@@ -0,0 +1,320 @@
+"""lda.py"""
+# Helper functions
+import csv
+import pdb
+from collections import OrderedDict
+import joblib
+import random
+from operator import itemgetter
+from numpy import geomspace
+import argparse
+import os
+import sys
+import logging
+
+# Gensim functions for topic modeling
+from gensim.corpora import Dictionary
+from gensim.models.ldamulticore import LdaMulticore
+from gensim.models.coherencemodel import CoherenceModel
+import gensim.downloader
+
+# Utility functions provided in CS230 project template
+from model.utils import Params, set_logger
+
+# Global settings
+csv.field_size_limit(sys.maxsize)
+PYTHON = sys.executable
+
+# Initialize argparse parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--parent_dir', default='experiments/num_topics',
+                    help="Directory containing params.json")
+parser.add_argument('--train_path', default='data/processed/train/train_200.csv',
+                    help="File containing test data")
+parser.add_argument('--hyperparameter', default='num_topics',
+                    help="Hyperparameter to tune")
+parser.add_argument('--dev_path', default='data/processed/dev/dev_200.csv',
+                    help="File containing dev data")
+
+def process_data(path, params, dictionary=None):
+    """Prepare data for LDA model and filter out most uncommon/common words"""
+    # Read in the data
+    logging.info("Reading in the data...")
+    data = path
+    docs = []
+    titles = []
+
+    with open(data, 'r') as f:
+        csv_text = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar="", escapechar="\\")
+        csv.field_size_limit(sys.maxsize)
+        
+        counter = 0
+        for row in csv_text:
+            # Skip the header row
+            if row != ['doc_id', 'place', 'date', 'doc_title', 'doc_type', 'doc_text']:
+                tokens = []
+                # Reformat the incoming text
+                text = row[-1][2:-2].replace("'", "").split(",")
+                title = '{}-{}'.format(row[0], counter)
+                for token in text:
+                    token = token.strip()
+                    if token != ' ':
+                        tokens.append(token)
+                docs.append(tokens)
+                titles.append(title)
+    logging.info("Done reading in the data.")
+    
+    if dictionary is None:
+        # Create a dictionary representation of the documents
+        # if there is not already one
+        logging.info("Creating dictionary...")
+        dictionary = Dictionary(docs[1:])
+        # We treat no_below and no_above as hyperparameters to tune.
+        dictionary.filter_extremes(no_below=params.no_below, no_above=params.no_above)
+        logging.info("Done creating dictionary.")
+
+    # Create a bag-of-words representation of the documents.
+    logging.info("Creating the corpus...")
+    corpus_long = [dictionary.doc2bow(doc) for doc in docs]
+    # Remove empty lists created because of filter_extremes
+    corpus = list(filter(None, corpus_long))
+    logging.info("Done creating the corpus.")
+    
+    return corpus, dictionary, titles
+
+def train_lda(corpus, params, dictionary):
+    """Train LDA model according to provided params"""
+    # Set training parameters.
+    num_topics = params.num_topics
+    chunksize = params.chunksize
+    passes = params.passes
+    iterations = params.iterations
+    # This evaluates perplexity. We're concerned 
+    # with coherence, so we leave this as None.
+    eval_every = None
+    decay = params.decay  
+
+    # Make an index to word dictionary.
+    logging.info("Mapping ids to words...")
+    temp = dictionary[0]
+    id2word = dictionary.id2token
+    logging.info("Done mapping ids to words.")
+
+    logging.info("Making the LDA model...")
+    lda = LdaMulticore(
+        corpus=corpus,
+        id2word=id2word,
+        workers=3,
+        chunksize=chunksize,
+        alpha='asymmetric', # If low: Each document is represented by only a few topics
+        eta='auto', # If low: Each topic is only represented by a few words
+        decay=decay,
+        iterations=iterations,
+        num_topics=num_topics,
+        passes=passes,
+        eval_every=eval_every,
+        random_state=230,
+        per_word_topics=True
+    )
+    logging.info("Done making the LDA model.")
+    return lda
+
+def log_results(lda, params, corpus, dictionary, train_flag):
+    """Print topics, calculate coherence and export results to csv."""
+    if train_flag:
+        logging.info("Logging training results...")
+    else:
+        logging.info("Logging dev set results...")
+    # Log top words from each topic and topic coherence scores 
+    top_topics = lda.top_topics(corpus)
+    lda.print_topics(num_topics=-1, num_words=20)
+    if train_flag:
+        logging.info("Finished logging training results...")
+    else:
+        logging.info("Finished logging dev results...")
+
+    logging.info("Evaluating average topic coherence...")
+    # Compute average topic coherence
+    # We want topic coherence as close to 0 as possible
+    avg_topic_coherence = sum([t[1] for t in top_topics]) / params.num_topics
+
+    # Calculate UMass coherence as a sanity check
+    # This should be identical to avg_topic_coherence
+    cm = CoherenceModel(model=lda, corpus=corpus, coherence='u_mass')
+    umass_coherence = cm.get_coherence() 
+
+    # Calculate GloVe coherence score
+    glove_vectors = gensim.downloader.load('glove-wiki-gigaword-50')
+    cm = CoherenceModel(model=lda, corpus=corpus, dictionary=dictionary, coherence='c_w2v', keyed_vectors=glove_vectors)
+    glove_coherence = cm.get_coherence()
+
+    # Put this data into a dictionary to save
+    metadata_dict = OrderedDict([
+            ('avg_umass_coherence', avg_topic_coherence),
+            ('umass_coherence', umass_coherence),
+            ('glove_coherence', glove_coherence),
+            ('num_topics', params.num_topics),
+            ('chunksize', params.chunksize),
+            ('passes', params.passes),
+            ('no_above_list', params.no_above),
+            ('no_below_list', params.no_below),
+            ('decay', params.decay)
+        ])
+
+    # Check if csv exists
+    if train_flag:
+        csv_path = os.path.join(args.parent_dir, 'train_results_200.csv')
+    else:
+        csv_path = os.path.join(args.parent_dir, 'dev_results_200.csv')
+    new_file = not os.path.exists(csv_path)
+
+    # Write out our results
+    with open(csv_path, 'a') as out_file:       
+        dict_writer = csv.DictWriter(out_file, metadata_dict.keys())
+        if new_file:
+            dict_writer.writeheader()
+        dict_writer.writerow(metadata_dict)
+    logging.info("Average topic coherence saved.")
+    
+    # Save the model
+    if train_flag:
+        logging.info("Saving model...")
+        model_name = 'num_topics_{}_chunksize_{}_passes_{}_no_above_{}_no_below_{}_decay_{}.jl'.format(
+            params.num_topics,
+            params.chunksize,
+            params.passes,
+            params.no_above,
+            params.no_below,
+            params.decay
+            )
+        joblib.dump(lda, os.path.join(args.parent_dir, model_name))
+        logging.info("Model saved.")
+
+def eval_results(dev_path, lda, dictionary, params):
+    """Calculate coherence on unseen documents and print topics for unseen documents."""
+    logging.info("Evaluating results on dev set...")
+    corpus, _, titles = process_data(path=dev_path, params=params, dictionary=dictionary)
+    # Get the topic probability distribution for each document in dev
+    log_results(lda, params, corpus, dictionary, False)
+    
+    # Get main topic in each new document
+    # Adapted from https://www.machinelearningplus.com/nlp/topic-modeling-visualization-how-to-present-results-lda-models/
+    for i, row_list in enumerate(lda[corpus]):
+
+        row = row_list[0]        
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the 3 most-dominant topics for each document
+        # Also get the weight of the topic in the document
+        logging.info("Original doc: " + titles[i])
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j < 2:
+                words = lda.show_topic(topicid=topic_num, topn=30)
+                topic_keywords = ", ".join([word for word, prop in words])
+                logging.info("Topic number: " + str(topic_num))
+                logging.info("Topic probability: " + str(round(prop_topic,4)))
+                logging.info("Topic keywords: " + str(topic_keywords))
+                
+    logging.info("Finished evaluating results on dev set.")
+    
+def run_search(search_param, args, params):
+    """Train a model and evaluate the model for a set of parameters during parameter search"""
+    # Define unique job name
+    job_name = "{}_{}".format(args.hyperparameter, search_param)
+
+    # Set the logger
+    set_logger(os.path.join(args.parent_dir, job_name + '.log'))
+
+    # Train the model
+    corpus, dictionary, _ = process_data(path=args.train_path, params=params, dictionary=None)
+    lda = train_lda(corpus, params, dictionary)
+
+    # Save results
+    log_results(lda, params, corpus, dictionary, True)
+
+    # Evaluate the model on dev set
+    eval_results(dev_path=args.dev_path, lda=lda, dictionary=dictionary, params=params)
+
+if __name__ == "__main__":
+    random.seed(230)
+    
+    # Load the "reference" parameters from parent_dir json file
+    args = parser.parse_args()
+    json_path = os.path.join(args.parent_dir, 'params.json')
+    assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
+    params = Params(json_path)
+
+    # Perform hypersearch over one parameter at a time
+    num_topics_list = geomspace(start=10, stop=30, num=15, dtype='int16')
+    chunksizes = [1152, 1216, 1280, 1344, 1408]
+    passes = geomspace(start=30, stop=50, num=15, dtype='int16') # Number of epochs
+    no_above_list = geomspace(start=0.4, stop=1, num=15)# Filter out words that occur in more than X/total documents
+    no_below_list = [1, 5, 10, 15, 20, 25] # Filter out words that occur in less than X documents
+    decay = geomspace(start=0.5, stop=1, num=10)
+    
+    if args.hyperparameter == 'num_topics':
+        search_params = num_topics_list
+        for search_param in search_params:
+            # Modify the relevant parameter in params
+            params.num_topics = search_param
+            run_search(search_param, args, params)
+    
+    elif args.hyperparameter == 'chunksize':
+        search_params = chunksizes
+        for search_param in search_params:
+            # Modify the relevant parameter in params
+            params.chunksize = search_param 
+            run_search(search_param, args, params)
+    
+    elif args.hyperparameter == 'no_above':
+        search_params = no_above_list
+        for search_param in search_params:
+            # Modify the relevant parameter in params
+            params.no_above = search_param 
+            run_search(search_param, args, params)
+
+    elif args.hyperparameter == 'no_below':
+        search_params = no_below_list
+        for search_param in search_params:
+            # Modify the relevant parameter in params
+            params.no_below = search_param 
+            run_search(search_param, args, params)
+            
+    elif args.hyperparameter == 'passes':
+        search_params = passes
+        for search_param in search_params:
+            # Modify the relevant parameter in params
+            params.passes = search_param 
+            run_search(search_param, args, params)
+    
+    elif args.hyperparameter == 'decay':
+        search_params = decay
+        for search_param in search_params:
+            # Modify the relevant parameter in params
+            params.decay = search_param 
+            run_search(search_param, args, params)
+
+# https://radimrehurek.com/gensim/auto_examples/core/run_similarity_queries.html
+# corpus = [dictionary.doc2bow(text) for text in texts]
+# doc = "Human computer interaction"
+# vec_bow = dictionary.doc2bow(doc.lower().split())
+# vec_lsi = lsi[vec_bow]  # convert the query to LSI space
+# print(vec_lsi)
+# from gensim import similarities
+# index = similarities.MatrixSimilarity(lsi[corpus])
+# sims = index[vec_lsi]  # perform a similarity query against the corpus
+# print(list(enumerate(sims)))  # print (document_number, document_similarity) 2-tuples
+#     sims = sorted(enumerate(sims), key=lambda item: -item[1])
+# for doc_position, doc_score in sims:
+#     print(doc_score, documents[doc_position])
+# potential viz: https://stackoverflow.com/questions/42995073/displaying-topics-associated-with-a-document-query-in-gensim
+
+# >>>
+# >>> # Load a potentially pretrained model from disk.
+# >>> lda = LdaModel.load(temp_file)
+# # then reload it with
+#    lda_model = joblib.load('lda_model.jl')
+
+# DOCS
+# https://radimrehurek.com/gensim/models/ldamodel.html
+# https://radimrehurek.com/gensim/models/ldamodel.html
+# Useful viz inspiration: https://www.machinelearningplus.com/nlp/topic-modeling-python-sklearn-examples/#10diagnosemodelperformancewithperplexityandloglikelihood
